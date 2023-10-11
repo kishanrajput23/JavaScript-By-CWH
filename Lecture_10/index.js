e7da353bceba254c5183cd0afb7f87a7fef38f2b
Kishan Kumar Rai <kishan.rai99693@gmail.com>
2023-10-11 14:54:56 +0530

    While Loops in JS

---
+++ Lecture_10/index.js
@@ -0,0 +1,19 @@
+// while loop
+let n  = prompt("Enter the value of n");
+n = Number.parseInt(n);
+let  i = 0;
+while (i<n) {
+    console.log(i);
+    i++;
+}
+
+// do while loop
+let a  = prompt("Enter the value of n");
+a = Number.parseInt(a);
+let  j = 10;
+do {
+    console.log(j);
+    j++;
+}
+while (j<a) {
+}
