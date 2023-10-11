7a5477118ce7b5f69c5790c11123c26e13c7a0a7
Kishan Kumar Rai <kishan.rai99693@gmail.com>
2023-10-11 14:51:57 +0530

    const, let and var

---
+++ Lecture_03/index.js
@@ -0,0 +1,16 @@
+console.log("Lecture 03");
+
+var a = "Kishan";
+var a = 10;
+console.log(a);     //The value of a has been updated now from kishan to 10
+
+const author = "Kishan";
+// let author = "shyam"  --> This line will throws an error because const can't be redefined.
+
+let b = 20;
+// let b = 400;  -->This will throws an error as we can't define same variable usin let
+console.log(b);
+
+
+let c = null;
+console.log(c);
