0c283b932bc7ee71cc4cad290fc15b241696e908
Kishan Kumar Rai <kishan.rai99693@gmail.com>
2023-10-11 14:53:20 +0530

    Operators and Expressions

---
+++ Lecture_06/index.js
@@ -0,0 +1,47 @@
+console.log("Lecture 06");
+
+// Arithmetic Operators
+let a = 10;
+let b = 5;
+console.log("a + b = ", a+b);
+console.log("a - b = ", a-b);
+console.log("a * b = ", a*b);
+console.log("a / b = ", a/b);
+console.log("a ** b = ", a**b);
+console.log("a % b = ", a%b);
+console.log("a++ = ", a++);
+console.log("a-- = ", a--);
+console.log("++a = ", ++a);
+console.log("--b = ", --a);
+
+// Assignment Operators
+let n = 10;
+n += 5;
+console.log("n is now : ", n);
+
+n -= 5;
+console.log("n is now : ", n);
+
+n *= 5;
+console.log("n is now : ", n);
+
+n /= 5;
+console.log("n is now : ", n);
+
+// Comparison Operators
+let p = 5;
+let q = 10;
+console.log("p == q", p==q);
+console.log("p != q", p!=q);
+console.log("p === q", p==q);
+console.log("p !== q", p!=q);
+console.log("p > q", p>q);
+console.log("p < q", p<q);
+
+// Logical Operators
+let x = 5;
+let y = 6;
+console.log(x<y && x==5);
+console.log(x<y || x==10);
+console.log(!false);
+console.log(!true);
