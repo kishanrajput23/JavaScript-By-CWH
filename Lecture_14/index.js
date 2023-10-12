d20df8cdfba64a70b018c14f3bd89389d8866007
Kishan Kumar Rai <kishan.rai99693@gmail.com>
2023-10-12 11:48:33 +0530

    Strings Methods

---
+++ Lecture_14/index.js
@@ -0,0 +1,23 @@
+let name = "kishan bhai";
+console.log(name);
+
+console.log(name.length);
+
+console.log(name.toUpperCase());
+
+console.log(name.toLowerCase());
+
+console.log(name.slice(2));
+
+console.log(name.slice(2, 4));
+
+let newName = name.replace("bhai", "bhau");
+console.log(newName);
+
+let name1 = "Kishan";
+let name2 = "Rajput";
+console.log(name1.concat(name2));
+
+let str = "            HELLO JI...        ";
+console.log(str);
+console.log(str.trim());
