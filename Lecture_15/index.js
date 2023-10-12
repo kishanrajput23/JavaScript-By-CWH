da3807fd0535530ff30d425f2feb5f2a5746f085
Kishan Kumar Rai <kishan.rai99693@gmail.com>
2023-10-12 12:02:46 +0530

    Chapter 4 Practice Set

---
+++ Lecture_15/index.js
@@ -0,0 +1,21 @@
+// chapter 4 question 1
+console.log("har\"".length);
+
+// chapter 4 question 2
+let str = "Today is thrusday.";
+console.log(str.includes("is"));
+console.log(str.startsWith("Today"));
+console.log(str.endsWith("y."));
+
+// chapter 4 question 3
+let sentence = "MY NAME IS KISHAN.";
+console.log(sentence.toLowerCase());
+
+// chapter 4 question 4
+let str1 = "Please give Rs 1000";
+console.log(str1.slice(15));
+
+// chapter 4 question 5
+let str2 = "Please give Rs 1000";
+// str2[4] = "k";   this will not change the string as strings are immutable.
+console.log(str2);
