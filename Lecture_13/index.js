dbef5a017df2feb38b9736440f351f57e69bca5c
Kishan Kumar Rai <kishan.rai99693@gmail.com>
2023-10-12 11:31:15 +0530

    Introduction to Strings

---
+++ Lecture_13/index.js
@@ -0,0 +1,21 @@
+let a = "Kishan";
+console.log(a);
+console.log(a.length)
+console.log(a[0]);
+
+let friend = 'rohit';
+console.log(friend);
+
+// Template literals
+let boy1 = "Kishan";
+let boy2 = "Rohit";
+
+let sentence = `${boy2} is friend of ${boy1}.`;
+console.log(sentence);
+
+// Escape Sequence Characters
+let fruit1 = 'Bana\'na';
+console.log(fruit1);
+
+let fruit2 = "Bana\"na";
+console.log(fruit2);
