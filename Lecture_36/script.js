ce8745870cf9475bb61dff13f35927b00f579f3a
Kishan Kumar Rai <kishan.rai99693@gmail.com>
2023-10-30 15:07:16 +0530

    Searching the DOM

---
+++ Lecture_36/script.js
@@ -0,0 +1,10 @@
+// change the card title to red
+let ctitle = document.getElementsByClassName("card-title")[0];
+ctitle.style.color = "red";
+
+let ctext = document.getElementById("card-text");
+ctext.style.color = "green";
+
+console.log(document.getElementsByTagName("a"));
+console.log(document.querySelector(".card").getElementsByTagName("a"));
+console.log(document.getElementsByName("search"));
